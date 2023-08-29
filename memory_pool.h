c784e7190d164f3352fb66eaa2ca981a4e6eba35
guohong <xieg678@gmail.com>
2023-08-29 19:23:13 +0800

    final version

---
+++ memory_pool.h
@@ -0,0 +1,88 @@
+#ifndef RYAN_MEMORY_POOL_MEMORY_POOL_H_
+#define RYAN_MEMORY_POOL_MEMORY_POOL_H_
+
+#include <cstddef>  //size_t
+
+//#define PAGE_SIZE 4096
+//#define MP_ALIGNMENT 16
+//#define mp_align(n, alignment) (((n) + (alignment - 1)) & ~(alignment - 1))
+//#define mp_align_ptr(p, alignment) \
+  (void*)((((size_t)p) + (alignment - 1)) & ~(alignment - 1))
+constexpr size_t PAGE_SIZE = 4096;
+constexpr size_t MP_ALIGNMENT = 16;
+
+inline constexpr size_t mp_align(size_t n, size_t alignment) {
+    return (n + alignment - 1) & ~(alignment - 1);
+}
+
+inline void* mp_align_ptr(void* p, size_t alignment) {
+    return reinterpret_cast<void*>((reinterpret_cast<size_t>(p) + alignment - 1) & ~(alignment - 1));
+}
+
+
+
+struct SmallNode {
+  unsigned char* end_;      // 该块的结尾
+  unsigned char* last_;     // 该块目前使用位置
+  unsigned int quote_;      // 该块被引用次数
+  unsigned int failed_;     // 该块失效次数
+  struct SmallNode* next_;  // 指向下一个块
+};
+
+struct LargeNode {
+  void* address_;           // 该块的起始地址
+  unsigned int size_;       // 该块大小
+  struct LargeNode* next_;  // 指向下一个大块
+};
+
+struct Pool {
+  LargeNode* large_list_;  // 管理大块内存链表
+  SmallNode* head_;        // 头节点
+  SmallNode*
+      current_;  // 指向当前分配的块，这样可以避免遍历前面已经不能分配的块
+};
+
+class MemoryPool {
+ public:
+  /// @brief 默认构造，真正初始化工作交给 createPool
+  MemoryPool() = default;
+
+  /// @brief 默认析构，真正销毁工作交给 destroyPool
+  ~MemoryPool() = default;
+
+  /// @brief 初始化内存池，为 pool_ 分配 PAGE_SIZE 内存
+  void CreatePool();
+
+  /// @brief 销毁内存池，遍历大块内存和小块内存且释放它们
+  void DestroyPool();
+
+  /// @brief 申请内存的接口，内部会判断创建大块还是小块内存
+  /// @param[in] size 分配内存大小
+  void* malloc(size_t size);
+
+  /// @brief 申请内存且将内存清零，内部调用 malloc
+  /// @param[in] size 分配内存大小
+  void* Calloc(size_t size);
+
+  /// @brief 释放内存指定内存
+  /// @param[in] p 释放内存头地址
+  void FreeMemory(void* p);
+
+  /// @brief 重置内存池
+  void ResetPool();
+
+  Pool* GetPool() { return pool_; }
+
+ private:
+  /// @brief 分配大块节点，被 malloc 调用
+  /// @param[in] size 分配内存大小
+  void* MallocLargeNode(size_t size);
+
+  /// @brief 分配小块节点，被 malloc 调用
+  /// @param[in] size 分配内存大小
+  void* MallocSmallNode(size_t size);
+
+  Pool* pool_ = nullptr;
+};
+
+#endif  // RYAN_MEMORY_POOL_MEMORY_POOL_H_
---
+++ test/memory_pool_test.cc
@@ -0,0 +1,148 @@
+#include "../memory_pool.h"
+
+#include <cstdio>
+#include <cstdlib>
+#include <chrono>
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+void ShowInfo(MemoryPool* Mpool, char* str) {
+  printf("\r\n\r\n------start pool status %s------\r\n\r\n", str);
+  Pool* pool = Mpool->GetPool();
+  SmallNode* head = nullptr;
+  int i = 1;
+  for (head = pool->head_; head; head = head->next_, i++) {
+    if (pool->current_ == head) {
+      printf("current==>第%d块\n", i);
+    }
+    if (i == 1) {
+      printf(
+          "the %02dth small block  used:%4ld  remaining:%4ld  refer:%4d  "
+          "failed:%4d\n",
+          i, (unsigned char*)head->last_ - (unsigned char*)pool,
+          head->end_ - head->last_, head->quote_, head->failed_);
+    } else {
+      printf(
+          "the %02dth small block  used:%4ld  remaining:%4ld  refer:%4d  "
+          "failed:%4d\n",
+          i, (unsigned char*)head->last_ - (unsigned char*)head,
+          head->end_ - head->last_, head->quote_, head->failed_);
+    }
+  }
+
+  LargeNode* large;
+  i = 0;
+  for (large = pool->large_list_; large; large = large->next_, i++) {
+    if (large->address_ != nullptr) {
+      printf("the %dth large block  size=%d\n", i, large->size_);
+    }
+  }
+  printf("\r\n\r\n------end pool status------\r\n\r\n");
+}
+
+void test1() {
+  printf("sizeof(Pool) = %ld sizeof(SmallNode) = %ld\n", sizeof(Pool),
+         sizeof(SmallNode));
+
+  vector<void*> memoryVector(50);
+  MemoryPool pool;
+  pool.CreatePool();
+
+  // 申请30个512字节的内存块
+  for (int i = 0; i < 50; i++) {
+    memoryVector[i] = pool.malloc(512);
+  }
+  ShowInfo(&pool, "apply for 50 blocks of 512 bytes");
+
+  // 销毁30个512字节的内存块
+  for (int i = 0; i < 50; i++) {
+    pool.FreeMemory(memoryVector[i]);
+  }
+  ShowInfo(&pool, "destroy 50 blocks of 512 bytes");
+
+  // 申请30个5120字节的内存块
+  for (int i = 0; i < 30; i++) {
+    memoryVector[i] = pool.malloc(5120);
+  }
+  ShowInfo(&pool, "apply for 30 blocks of 512 bytes");
+
+  // 销毁30个5120字节的内存块
+  for (int i = 0; i < 30; i++) {
+    pool.FreeMemory(memoryVector[i]);
+  }
+  ShowInfo(&pool, "destroy 30 blocks of 512 bytes");
+
+  pool.ResetPool();
+  ShowInfo(&pool, "reset the memory pool");
+
+  pool.DestroyPool();
+}
+
+#define ALLOCATE_COUNT 1000000
+#define ALLOCATE_SIZE 10
+
+void test2() {
+  // vector<void*> memoryVector(100);
+  void* memory[ALLOCATE_COUNT];
+
+  cout << "use malloc free time" << endl;
+  cout << "--------------------" << endl;
+  auto start = std::chrono::steady_clock::now();
+  for (int i = 0; i < ALLOCATE_COUNT; i++) {
+    memory[i] = malloc(10);
+  }
+  auto end = std::chrono::steady_clock::now();
+  cout << "the time cost of construct: "
+       << std::chrono::duration_cast<std::chrono::microseconds>(end - start)
+              .count()
+       << "us" << endl;
+
+  start = std::chrono::steady_clock::now();
+  for (int i = 0; i < ALLOCATE_COUNT; i++) {
+    free(memory[i]);
+  }
+  end = std::chrono::steady_clock::now();
+  cout << "the time cost of destroy: "
+       << std::chrono::duration_cast<std::chrono::microseconds>(end - start)
+              .count()
+       << "us" << endl;
+  cout << "--------------------" << endl;
+  cout << endl;
+
+  MemoryPool pool;
+  pool.CreatePool();
+
+  cout << "use MemoryPool time" << endl;
+  cout << "--------------------" << endl;
+  start = std::chrono::steady_clock::now();
+  for (int i = 0; i < ALLOCATE_COUNT; i++) {
+    memory[i] = pool.malloc(10);
+  }
+  end = std::chrono::steady_clock::now();
+  cout << "the time cost of construct: "
+       << std::chrono::duration_cast<std::chrono::microseconds>(end - start)
+              .count()
+       << "us" << endl;
+
+  start = std::chrono::steady_clock::now();
+  pool.DestroyPool();
+  end = std::chrono::steady_clock::now();
+  cout << "the time cost of destroy: "
+       << std::chrono::duration_cast<std::chrono::microseconds>(end - start)
+              .count()
+       << "us" << endl;
+  cout << "--------------------" << endl;
+  cout << endl;
+}
+
+int main() {
+  // test1();
+
+  test2();
+
+  return 0;
+}
